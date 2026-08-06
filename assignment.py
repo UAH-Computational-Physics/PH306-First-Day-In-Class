413101ff4a1ad90c663001bcc62df48c07b98a78
Will Waldron <Will.Waldron@uah.edu>
2026-08-06 14:52:52 -0500

    Added constant for earth gravity

--- assignment.py
+++ assignment.py
@@ -10,6 +10,10 @@ from astropy import units as u
 from astropy.units import Quantity as Q
 
 
+# --- Constants --- #
+EARTH_GRAVITY = u.Quantity(9.81, u.m / u.s**2)  # Earth's gravitational acceleration
+
+
 # --- Functions to Implement --- #
 def distance_traveled(v0: Q[u.m/u.s], a: Q[u.m/u.s**2], t: Q[u.s]) -> Q[u.m]:
     """Return the displacement for constant acceleration.
@@ -27,14 +31,15 @@ def free_fall_height(
     y0: Q[u.m],
     t: Q[u.s],
     v0: Q[u.m/u.s] = u.Quantity(0.0, u.m / u.s),
-    g: Q[u.m/u.s**2] = u.Quantity(9.81, u.m / u.s**2),
+    g: Q[u.m/u.s**2] = EARTH_GRAVITY,
 ) -> Q[u.m]:
     """Return the height of an object in vertical motion.
     """
     raise NotImplementedError("Implement free_fall_height")
 
 
-def projectile_range(v0: Q[u.m/u.s], th0: Q[u.deg], g: Q[u.m/u.s**2] = u.Quantity(9.81, u.m / u.s**2)) -> Q[u.m]:
+
+def projectile_range(v0: Q[u.m/u.s], th0: Q[u.deg], g: Q[u.m/u.s**2] = EARTH_GRAVITY) -> Q[u.m]:
     """Return the ideal range of a projectile launched and landing at the same height.
     """
     raise NotImplementedError("Implement projectile_range")
